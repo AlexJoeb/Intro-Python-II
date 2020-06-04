8c730932d77899a921bcb39927e7002b81a9642a
Alexander Besse <alexanderjoeb@hotmail.com>
2020-06-04 12:18:04 -0500

    Base of game and inv system built out.

--- src/adv.py
+++ src/adv.py
@@ -1,37 +1,42 @@
-from room import Room
+from room import Room, Chest
+from player import Player
+from items import Item
+from os import system, name
 
-# Declare all the rooms
+brone_coin = Item("Bronze Coin")
+silver_coin = Item("Silver Coin")
+gold_coin = Item("Gold Coin")
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", Chest([])),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", Chest([brone_coin])),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", Chest([])),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", Chest([silver_coin])),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", Chest([silver_coin, gold_coin])),
 }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].n = room['foyer']
+room['foyer'].s = room['outside']
+room['foyer'].n = room['overlook']
+room['foyer'].e = room['narrow']
+room['overlook'].s = room['foyer']
+room['narrow'].w = room['foyer']
+room['narrow'].n = room['treasure']
+room['treasure'].s = room['narrow']
 
 #
 # Main
@@ -39,13 +44,97 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+name = input("What is your name?\n")
+if name == 'exit' or name == 'q': done = 1
+
+player = Player(name, room['outside'])
+
+print(f'You\'ve started outside.')
+
+# -> Gets the short nick-name of the room.
+def get_room_short_name(room_name):
+    if room_name == 'Outside Cave Entrance': return 'outside'
+    elif room_name == 'Grand Overlook': return 'overlook'
+    elif room_name == 'Foyer': return 'foyer'
+    elif room_name == 'Narrow Passage': return 'narrow'
+    elif room_name == 'Treasure Chamber': return 'treasure'
+    else: return 'outside'
+
+# -> Returns the full name of a direction given a singluar abv.
+def full_dir(direction):
+    if len(direction) > 1: return direction
+    else:
+        if direction == 'n': return 'north'
+        elif direction == 'e': return 'east'
+        elif direction == 's': return 'south'
+        elif direction == 'w': return 'west'
+        else: return 'no direction found.'
+
+# -> Get the next room given a current room and direction.
+def get_next_room(current, direction):
+    current = current.lower()
+    direction = direction.lower()
+    short_dir = direction[0]
+    next_room = None
+    try:
+        next_room = room[current].get_room(short_dir)
+        return next_room
+    except AttributeError:
+        return None
+
+def item_in_room_chest(item_name):
+    for item in player.room.chest.items:
+        print(item.name, item_name)
+        if item.name.lower() == item_name.lower(): return item
+        else: continue
+    return None
+    
+def item_in_backpack(item_name):
+    for item in player.backpack.items:
+        if item.name.lower() == item_name.lower(): return item
+        else: continue
+    return None
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+done = 0
+
+# ! Game Logic Below
+while(done == 0):
+    clear()
+    print(f'You are now in: {player.room}')
+    print('=-=')
+    current_room = get_room_short_name(player.room.name)
+    room_obj = room[current_room]
+
+    if len(room_obj.chest.items) > 0: 
+        print('This room has a chest with items, type "open chest" to open the chest.')
+
+    command = input("What would you like to do next?\n")
+    
+    if command == 'exit' or command == 'q':
+        done = 1
+        continue
+    elif current_room == 'overlook' and command[0] == 'n':
+        print("You've stepped into the dark abyss to your death.\n\nGame Over.")
+        done = 1
+    elif command.split(' ')[0].lower() == 'open':
+        second_arg = command.split(' ')[1].lower()
+        if second_arg == 'chest':
+            room_obj.chest.open(player)
+        elif second_arg == 'backpack':
+            player.backpack.open(player)
+        continue
+
+    next_room = get_next_room(current_room, command);
+    
+    if next_room == None:
+        print(f'Theres nothing to the {full_dir(command)}.')
+        continue
+    else:
+        player.room = next_room
+    
--- src/room.py
+++ src/room.py
@@ -1,2 +1,93 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+from items import Item
+
+class Chest:
+    def __init__(self, items):
+        self.items = items
+
+    def __str__(self):
+        contents = ', '.join([item.name for item in self.items])
+        return f"You open the chest and find: {contents}"
+
+    def contains(self, item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower(): return item
+            else: continue
+        return None
+
+    def open(self, player):
+        if len(self.items) <= 0:
+            print('This chest is empty.');
+            while(1):
+                command = input('Type "close" to close the chest.')
+                if command == 'close':
+                    break
+                else: continue
+            return None
+        else:
+            printed = 0
+            while(1):
+                # What does the player find, remove it from the chest and add it to the player backpack.
+
+                # Print contents of the chest.
+
+                if(len(self.items)):
+                    print(self)
+                else:
+                    print('The chest is now empty.')
+
+                command = None
+                if printed == 0:
+                    command = input('Type "close" to close the chest or "get [item name]" to pick something up.\n>> ')
+                else: command = input('>> ')
+
+                if command.split(' ')[0] == 'close':
+                    break
+                elif command.split(' ')[0] == 'get':
+                    # get an item
+                    item_name = " ".join(command.split(' ')[1:])
+                    item_to_get = self.contains(item_name)
+                    if item_to_get == None:
+                        # ! Item does not exist.
+                        print("You've rummaged through the chest but can not find that item.")
+                        continue
+                    else:
+                        player.pickup_item(item_to_get)
+                        print(f"You picked up the {item_to_get.name}")
+                        continue
+            return None
+
+        if len(self.items) <= 0: 
+            print(f"The chest is empty.")
+            return None
+        else:
+            item_names = []
+
+            for item in self.items:
+                item_names.append(item.name)
+
+            print(f'You open the chest and find:\n{", ".join(item_names).strip(", ")}')
+
+class Room:
+    def __init__(self, name, desc, chest):
+        self.name = name
+        self.desc = desc
+        self.chest = chest
+
+    def __str__(self):
+        return f'{self.name}\n({self.desc})'
+
+    def get_room(self, direction):
+        if direction == 'n': return self.n
+        elif direction == 'e': return self.e
+        elif direction == 's': return self.s
+        elif direction == 'w': return self.w
+        else: return None
+
+    def add_item(self, item):
+        self.chest.items.append(Item(item.name))
+
+    def remove_item(self, item):
+        self.chest.items.remove(item)
